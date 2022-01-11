a6427a9e53ac1496f655ccdae0fd2031a070bb06
Artemis <artemisdevqa@gmail.com>
2022-01-11 00:20:26 -0700

    Few changes

--- Week2/DefiningFunctions/DefiningFunctions_Ex2.py
+++ Week2/DefiningFunctions/DefiningFunctions_Ex2.py
@@ -1,5 +1,6 @@
 def greeting(name, department):
     print("Welcome, " + name)
     print("You are part of " + department)
-    
+
 greeting("Blake", "IT Support")
+greeting("Ellis", "Software Engineer")

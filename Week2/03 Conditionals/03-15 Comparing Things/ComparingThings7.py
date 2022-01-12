43a963446cee6239aea85a9dc27c22a1dae2eba1
Artemis <artemisdevqa@gmail.com>
2022-01-12 15:18:37 -0700

    Comparison Operators

--- Week2/03 Conditionals/03-15 Comparing Things/ComparingThings7.py
+++ Week2/03 Conditionals/03-15 Comparing Things/ComparingThings7.py
@@ -1,2 +1,9 @@
 print("Yellow" > "Cyan" and "Brown" > "Magenta")
 print("Yellow" > "Cyan" or "Brown" > "Magenta")
+
+print(25 > 50 or 1 != 2)
+print(25 > 50 and 1 != 2)
+
+print(not 42 == "Answer")
+print(42 == "Answer")
+print(not 42 != "Answer")

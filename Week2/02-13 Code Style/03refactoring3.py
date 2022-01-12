9b0bd3b7b80b4e3bd496d90c324827678e5836ef
Artemis <artemisdevqa@gmail.com>
2022-01-11 17:15:12 -0700

    staging

---
+++ Week2/02-13 Code Style/03refactoring3.py
@@ -0,0 +1,5 @@
+def f1(x, y):
+	z = x*y  # the area is base*height
+	print("The area is " + str(z))
+
+f1(3,4)
---
+++ Week2/02-13 Code Style/04rectangle_area.py
@@ -0,0 +1,5 @@
+def rectangle_area(base, height):
+    area = base * height #area of a rectangle
+    print("The area is " + str(area))
+
+rectangle_area(5,6)

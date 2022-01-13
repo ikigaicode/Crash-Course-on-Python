4177b5b99e78c2a314d6ef3609becf00281ad335
Artemis <artemisdevqa@gmail.com>
2022-01-12 19:08:29 -0700

    A very complicated quiz of Conditionals and bytes.

---
+++ Week2/03 Conditionals/02-23 QuizConditionals/03-C-Q.py
@@ -0,0 +1,9 @@
+number = 10      #I added this line just to run and check the answer
+if number > 11:
+  print(0)
+elif number != 10:
+  print(1)
+elif number >= 20 or number < 12:
+  print(2)
+else:
+  print(3)

b83fe17a422f2f569ffc045499a74be9b6bcc678
Artemis <artemisdevqa@gmail.com>
2022-01-31 23:18:59 -0700

    Staging Week 3

---
+++ Week 4/W4-01 Strings/04-03 The Parts of a String/Ex 07 W4.py
@@ -0,0 +1,10 @@
+name = "Lisa Manoban"
+print(name[1])
+
+print(name[0])
+
+print(name[5])
+
+print(name[11])
+
+print(name[12])
---
+++ Week 4/W4-01 Strings/Question 2 W4.py
@@ -0,0 +1,6 @@
+def first_and_last(message):
+    return False
+
+print(first_and_last("else"))
+print(first_and_last("tree"))
+print(first_and_last(""))

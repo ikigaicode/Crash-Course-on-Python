105f28232983976e8cb17bf72bec73a996b16a7f
Artemis <artemisdevqa@gmail.com>
2022-02-02 01:51:43 -0700

    indexing

---
+++ Week 4/W4-01 Strings/04-05 Creating New Strings/Ex 19 W4.py
@@ -0,0 +1,15 @@
+pets = "Cats & Dogs"
+pets.index("&")
+print(pets.index("&"))
+
+pets.index("C")
+print(pets.index("C"))
+
+pets.index("Dog")
+print(pets.index("Dog"))
+
+pets.index("s")
+print(pets.index("s"))
+
+pets.index("x")
+print(pets.index("x"))

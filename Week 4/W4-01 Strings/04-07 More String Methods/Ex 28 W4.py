931454fdd97c5e482beed872970b5eff46529f8a
Artemis <artemisdevqa@gmail.com>
2022-02-03 01:30:39 -0700

    isnumeric() function

--- Week 4/W4-01 Strings/04-07 More String Methods/Ex 28 W4.py
+++ Week 4/W4-01 Strings/04-07 More String Methods/Ex 28 W4.py
@@ -1,4 +1,8 @@
 "Forest".isnumeric()
-print("Forest".endswith("rest"))
+print("Forest".isnumeric())
 
 "12345".isnumeric()
+print("12345".isnumeric())
+
+int("12345") + int("54321")
+print(int("12345") + int("54321"))

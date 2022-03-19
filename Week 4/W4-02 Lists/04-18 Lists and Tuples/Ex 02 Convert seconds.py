effd27a05415068eab26c26be79f81ba9e6a2147
Artemis <artemisdevqa@gmail.com>
2022-03-18 19:39:19 -0700

    what kind of list is this?

--- Week 4/W4-02 Lists/04-18 Lists and Tuples/Ex 02 Convert seconds.py
+++ Week 4/W4-02 Lists/04-18 Lists and Tuples/Ex 02 Convert seconds.py
@@ -3,3 +3,6 @@ def convert_seconds(seconds):
     minutes = (seconds - hours * 3600) // 60
     remaining_seconds = seconds - hours * 3600 - minutes * 60
     return hours. minutes, remaining_seconds
+
+result = convert_seconds(5000)
+type(result)

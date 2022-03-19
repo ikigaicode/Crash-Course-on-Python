37c2647c7e4055abede3ddf30a22c3f8a9c83dfe
Artemis <artemisdevqa@gmail.com>
2022-03-18 19:23:29 -0700

    Reading again about for loops chapter.

--- Week 4/W4-02 Lists/04-16 Modifying the Contents of a List/Question 2 -List W4.py
+++ Week 4/W4-02 Lists/04-16 Modifying the Contents of a List/Question 2 -List W4.py
@@ -9,16 +9,15 @@ def skip_elements(elements):
 	i = 0
 
 	# Iterate through the list
-	for ___
+	for element in elements:
 		# Does this element belong in the resulting list?
-		if ___
+		if i % 2 == 0:
 			# Add this element to the resulting list
-			___
+			new_list.append(element)
 		# Increment i
+		i += 1
 
-		___
-
-	return ___
+	return new_list
 
 print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
 print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']

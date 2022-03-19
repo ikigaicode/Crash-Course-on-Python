b648ff0049c07e76ecfd8b94108bed09134b4ca6
Artemis <artemisdevqa@gmail.com>
2022-03-18 19:48:14 -0700

    Instructions of the Q3

---
+++ Week 4/W4-02 Lists/04-18 Lists and Tuples/Question 3 - Tuple - Lists W4.py
@@ -0,0 +1,4 @@
+# Let's use tuples to store information about a file: its name,
+# its type and its size in bytes. Fill in the gaps in this code
+# to return the size in kilobytes (a kilobyte is 1024 bytes)
+# up to 2 decimal places.

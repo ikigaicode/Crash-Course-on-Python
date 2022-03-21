44e84ac63e5a7dad3ad8d9ce848fb30be9b4c2c4
Artemis <artemisdevqa@gmail.com>
2022-03-20 21:00:48 -0700

    Code of animals

--- Week 4/W4-02 Lists/04-20 Iterating over Lists and Tuples/Ex 02 - List of Animals.py
+++ Week 4/W4-02 Lists/04-20 Iterating over Lists and Tuples/Ex 02 - List of Animals.py
@@ -1,1 +1,6 @@
-animals =()
+animals =["Lion", "Zebra", "Dolphin", "Monkey"]
+chars = 0
+for animal in animals:
+    chars += len(animal)
+
+print("Total characters: {}, Average length: {}".format(chars, chars/len(animals)))
